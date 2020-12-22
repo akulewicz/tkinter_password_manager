84f3a7f5361a8a27939bf8e81f4030e74b43eed3
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2020-12-22 20:36:40 +0100

    Add password generator and copy password to the clipboard

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import tkinter
+import random
 from tkinter import messagebox
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+
+    password_input.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -13,7 +37,7 @@ def save_password():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oooooppsss", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {username} "
+        is_ok = messagebox.askokcancel(title=website, message=f"There are the details entered: \nEmail: {username}"
                            f"\n Password: {password} \nIs it ok to save?")
         if is_ok:
             with open('pass.txt', "a") as file:
@@ -52,7 +76,7 @@ password_label.grid(row=3, column=0)
 password_input = tkinter.Entry(width=33)
 password_input.grid(row=3, column=1)
 
-generate_button = tkinter.Button(text="Generate password")
+generate_button = tkinter.Button(text="Generate password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = tkinter.Button(text="Add", width=44, command=save_password)
